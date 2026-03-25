1c91e35fec1185a4ef5fddbdb1da137553c82bf3
Yash sharma <yashsharmaofficially@gmail.com>
2026-03-25 17:00:14 +0530

    Added signup,signin

--- apps/primary-backend/src/modules/auth/index.ts
+++ apps/primary-backend/src/modules/auth/index.ts
@@ -1,11 +1,31 @@
 import {Elysia} from 'elysia'
+import { AuthModel } from './models'
+import { AuthService } from './service'
 
 export const app = new Elysia({prefix:"auth"})
-    .post("/sign-up",({body})=>{
+    .post("/sign-up",async ({body})=>{
+        const userId = await AuthService.signup(body.email,body.password);
+        return {
+            id:userId
+        }
+
 
     },{
-        body:
+        body:AuthModel.signupSchema,
+        response:{
+            200:AuthModel.SignupResponseSchema,
+            400:AuthModel.signupFailedSchema
+        }
     })
-    .post("/sign-in",()=>{
-
+    .post("/sign-in",async ({body})=>{
+        const token = await AuthService.signin(body.email,body.password)
+        return {
+            token
+        }
+    },{
+        body:AuthModel.signinSchema,
+        response:{
+            200:AuthModel.SigninResponseSchema,
+           403:AuthModel.SigninFailedSchema
+        }
     })
--- apps/primary-backend/src/modules/auth/models.ts
+++ apps/primary-backend/src/modules/auth/models.ts
@@ -1,17 +1,45 @@
 import {t} from 'elysia'
 
 export namespace AuthModel {
-    const signinSchema = t.Object({
+    export const signinSchema = t.Object({
         email:t.String(),
         password:t.String()
 
     })
     export type signInSchema = typeof signinSchema.static
 
-    const SigninResponseSchema = t.Object({
+    export const SigninResponseSchema = t.Object({
         token:t.String()
     })
 
     export type SigninResponseSchema = typeof SigninResponseSchema.static;
+
+    export const SigninFailedSchema = t.Object({
+        message:t.Literal("error")
+    })
+    
+    export type SigninFailedSchema = typeof SigninFailedSchema.static;
+
+
+    //Signup 
+
+    export const signupSchema = t.Object({
+        email:t.String(),
+        password:t.String()
+
+    })
+    export type signupSchema = typeof signupSchema.static
+
+   export  const SignupResponseSchema = t.Object({
+        id:t.String()
+    })
+
+    export const signupFailedSchema = t.Object({
+        message:t.Literal("error")
+    })
+
+    export type SignupResponseSchema = typeof SignupResponseSchema.static;
+
+    export type SignupFailedSchema = typeof signupFailedSchema.static;
 
 }
