68cd7e9396dea187e52708da17c7e254403b5acc
Yash sharma <yashsharmaofficially@gmail.com>
2026-03-25 17:48:50 +0530

    prisma sucessfully

--- apps/primary-backend/src/modules/auth/service.ts
+++ apps/primary-backend/src/modules/auth/service.ts
@@ -1,9 +1,21 @@
+import {prisma} from "db"
 export abstract class AuthService {
-    static async signup(username:string,password:string):Promise<string>{
-        return "123"
+    static async signup(email:string,password:string):Promise<string>{
+        console.log("Creating user...")
+        const user = await prisma.user.create({
+            
+            data:{
+                email,
+                password,
+               name:""
+            }
+        })
+        return user.id.toString() 
+        
 
     }
-     static async signin(username:string,password:string):Promise<string>{
+    
+     static async signin(email:string,password:string):Promise<string>{
         return "token"
      }
 }
