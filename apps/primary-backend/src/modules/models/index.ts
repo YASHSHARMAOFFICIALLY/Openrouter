551f0570fcff58f6e5fb97cb83b0dfb1fb55e37e
Yash sharma <yashsharmaofficially@gmail.com>
2026-03-27 00:51:25 +0530

    models

---
+++ apps/primary-backend/src/modules/models/index.ts
@@ -0,0 +1,9 @@
+import Elysia from "elysia";
+import { ModelsModel } from "./models";
+import { ModelsService } from "./service";
+
+
+export const app = new Elysia ({prefix:"models"})
+    .get("/".async()=>{
+        const model = awa
+    })
---
+++ apps/primary-backend/src/modules/models/service.ts
@@ -0,0 +1,60 @@
+import {prisma} from "db"
+
+
+export abstract class ModelsService{
+    
+    static async getModels(){
+        const models = await prisma.model.findMany({
+            include:{
+                company:true
+            }
+        })
+
+        return models.map(model=>({
+            id:model.id.toString(),
+            name:model.name,
+            slug:model.slug,
+            comapny:{
+                id:model.company.id.toString(),
+                name:model.company.name,
+                website:model.company.website
+            }
+        }))
+    }
+
+
+
+
+    static async getProviders(){
+        const providers = await prisma.provider.findMany()
+
+        return providers.map(provider=>({
+            id:provider.id.toString(),
+            name:provider.name,
+            website:provider.Website
+        }))
+
+    }
+
+        static async getModelProviders(modelId: number) {
+        const mappings = await prisma.modelProviderMapping.findMany({
+            where: {
+                modelId
+            },
+            include: {
+                provider: true
+            }
+        })
+
+        return mappings.map(mapping => ({
+            id: mapping.id.toString(),
+            providerId: mapping.provider.id.toString(),
+            providerName: mapping.provider.name,
+            providerWebsite: mapping.provider.Website,
+            inputTokenCost: mapping.inputTokenCost,
+            outputTokenCost: mapping.outputTokenCost
+        }))
+    }
+
+
+}
