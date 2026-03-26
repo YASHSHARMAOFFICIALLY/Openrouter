03d7be3a6c777a8560aa0db23e2c12190daaabca
Yash sharma <yashsharmaofficially@gmail.com>
2026-03-26 11:50:43 +0530

    added file

--- apps/primary-backend/src/modules/apiKeys/index.ts
+++ apps/primary-backend/src/modules/apiKeys/index.ts
@@ -1,61 +1,84 @@
 import jwt from "@elysiajs/jwt";
-import Elysia, { status } from "elysia";
+import Elysia, { t } from "elysia";
 import { ApiKeyModel } from "./models";
 import { ApiKeyService } from "./service";
 
-export const app = new Elysia({prefix:"api-keys"})
-.use(
-    jwt({
-        name:'jwt',
-        secret : process.env.JWT_SECRET!
+export const app = new Elysia({ prefix: "api-keys" })
+    .use(
+        jwt({
+            name: 'jwt',
+            secret: process.env.JWT_SECRET!
+        })
+    )
+    .resolve(async ({ cookie: { auth }, status, jwt}) => {
+        if (!auth) {
+            return status(401)
+        }
 
+        const decoded = await jwt.verify(auth.value as string);
+
+        if (!decoded || !decoded.userId) {
+            return status(401)
+        }
+
+        return {
+            userId: decoded.userId as string
+        }
     })
-)
-// )
-    // .resolve(
-    //     async function verifyToken ({cookie:{auth},status,jwt}) => {
-
-    //     if(!auth ){
-    //         return status(401)
-    //     }
-    //     const decoded = jwt.verify(auth.value as string)
-
-    //     if(!decoded || !decoded.userId){
-    //         return status(401)
-
-    //     }
-    //     return {
-    //         userId:decoded.userId as string
-    //     }
-    //     })
-
-.post("/",async({userId,body})=>{
-   const {apiKey,id}  = await ApiKeyService.createApiKey(userId,Number(body.name))
-   return{
-    id,
-    apiKey
-   }
-
-},{
-    body:ApiKeyModel.CrateApiKeySchema,
-    response:{
-        200:ApiKeyModel.createAPiKeyResponse
-    }
-})
-
-.get("/",()=>{
-    const apikeys = await ApiKeyService.getApiKeys(Number(userId));
-    return{
-        apikeys
-    }
-},{
-    response:{
-        200:ApiKeyModel.getApiKeysResponseSchema
-    }
-})
-.post("/disable",()=>{
-
-})
-.delete("/",()=>{
-
-})
+    .post("/", async ({ userId, body }) => {
+        const { apiKey, id } = await ApiKeyService.createApiKey(body.name, Number(userId))
+        return {
+            id,
+            apiKey
+        }
+    }, {
+        body: ApiKeyModel.CrateApiKeySchema,
+        response: {
+            200: ApiKeyModel.createAPiKeyResponse
+        }
+    })
+    .get("/", async ({ userId }) => {
+        const apiKeys = await ApiKeyService.getApiKeys(Number(userId));
+        return {
+            apiKeys: apiKeys
+        }
+    }, {
+        response: {
+            200: ApiKeyModel.getApiKeysResponseSchema
+        }
+    })
+    .put("/", ({ body, userId, status }) => {
+        try {
+            ApiKeyService.updateApiKeyDisabled(Number(body.id), Number(userId), body.disabled);
+            return {
+                message: "Updated api key successfully"
+            }
+        } catch(e) {
+            return status(411, {
+                message: "Updating api key unsuccessful"
+            })
+        }
+    }, {
+        body: ApiKeyModel.updateApiKeySchema,
+        response: {
+            200: ApiKeyModel.updateApiKeyResponseSchema,
+            411: ApiKeyModel.disableApiKeyResponseFailedSchema
+        }
+    })
+    .delete("/:id", async ({ params: { id }, userId, status }) => {
+        try {
+            await ApiKeyService.delete(Number(id), Number(userId))
+            return {
+                message: "Api key deleted successfully"
+            }
+        } catch(e) {
+            return status(411,{
+                message: "Api key deletetion failed"
+            })
+        }
+    }, {
+        response: {
+            200: ApiKeyModel.deleteApiKeyResponseSchema,
+            411: ApiKeyModel.deleteApiKeyResponseFailedSchema
+        }
+    })
--- apps/primary-backend/src/modules/apiKeys/service.ts
+++ apps/primary-backend/src/modules/apiKeys/service.ts
@@ -2,54 +2,81 @@ import {prisma} from "db"
 const API_KEY_LENGTH = 32
 const ALPHABET_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 export abstract class ApiKeyService {
-    
-
-    
     static createRandomApiKey(){
-        let suffixKey = "";
-        for(let i = 0;i<API_KEY_LENGTH;i++){
-            const randomIndex = Math.floor(Math.random() * ALPHABET_SET.length)
-            suffixKey += ALPHABET_SET[randomIndex]
+        let suffix = ""
+        for (let i = 0;i<API_KEY_LENGTH;i++){
+            suffix += ALPHABET_SET[Math.floor(Math.random()*ALPHABET_SET.length)]
         }
-        return `sk-or-v1-${suffixKey}`
-
-        static async createApiKey(name:string,userId:number):Promise<
-        {
-            id:string,
-            apiKey:string
-        }>{
-            const apiKey = ApiKeyService.createRandomApiKey()
-            const apiKeydb = await prisma.apiKey.create({
-                data:{
-                    name,
-                    apiKey,
-                    userId 
-                    
-                }
-            })
-            return {
-                id:apiKeydb.id.toString(),
-                apiKey
-
-            }
-        }
-
-    
-
-    static async getApiKeys(userId:number){
-        const apiKeys = await prisma.apiKey.findMany({
-            where:{
-                userId:userId
-            }
-        })
-        return(apiKeys).map(apiKey => ({
-            id:apiKey.id,
-            name:apiKey.name,
-            creditConsumed:apiKey.creditConsumed,
-            lastused:apiKey.lastUsed,
-        }))
+        return `sk-or -${suffix}`
     }
 
 
+    static async createApiKey(name: string, userId: number): Promise<{
+        id: string,
+        apiKey: string
+    }> {
+
+        const apiKey = ApiKeyService.createRandomApiKey();
+        const apiKeyDb = await prisma.apiKey.create({
+            data: {
+                name, 
+                apiKey,
+                userId
+            }
+        })
+
+        return {
+            id: apiKeyDb.id.toString(),
+            apiKey
+        }
+    }
+
+
+     static async getApiKeys(userId: number) {
+        const apiKeys = await prisma.apiKey.findMany({
+            where: {
+                userId: userId,
+                deleted: false
+            }
+        })
+
+        return apiKeys.map(apiKey => ({
+            id: apiKey.id.toString(),
+            apiKey: apiKey.apiKey,
+            name: apiKey.name,
+            credisConsumed: apiKey.creditsConsumed,
+            lastUsed: apiKey.lastUsed,
+            disabled: apiKey.disbaled
+        }))
+    }
+
+    static async updateApiKeyDisabled(apiKeyId: number, userId: number, disabled: boolean) {
+        await prisma.apiKey.update({
+            where: {
+                id: apiKeyId,
+                userId
+            },
+            data: {
+                disabled
+            }
+        })
+    }
+
+     static async delete(id: number, userId: number) {
+        await prisma.apiKey.update({
+            where: {
+                id,
+                userId
+            },
+            data: {
+                deleted: true
+            }
+        })
+    }
 }
+
+
+
+    
+    
 }
