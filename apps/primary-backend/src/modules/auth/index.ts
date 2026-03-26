0f615a97f3816d9dfeb1219f90bf3d57d6c12cb8
Yash sharma <yashsharmaofficially@gmail.com>
2026-03-26 10:59:36 +0530

    fixed auth

--- apps/primary-backend/src/modules/auth/index.ts
+++ apps/primary-backend/src/modules/auth/index.ts
@@ -1,4 +1,4 @@
-import {Elysia} from 'elysia'
+import {Elysia,Cookie, status} from 'elysia'
 import { AuthModel } from './models'
 import { AuthService } from './service'
 import jwt from "@elysiajs/jwt"
@@ -6,17 +6,28 @@ import jwt from "@elysiajs/jwt"
 export const app = new Elysia({prefix:"auth"})
     .use(
         jwt({
+            name:'jwt',
             secret:process.env.JWT_SECRET!, 
-            name:"auth_token",
+           
             
         })
     )
-    .post("/sign-up",async ({body})=>{
-        const userId = await AuthService.signup(body.email,body.password);
-        return {
-            id:userId
+
+
+
+    .post("/sign-up",async ({body,status})=>{
+        try {
+                    const userId = await AuthService.signup(body.email,body.password);
+                     return {
+                    id:userId
         }
 
+        }catch(e){
+            console.log(e)
+            return status(400,{
+                message: "Error while Signing up"
+            })
+        }
 
     },{
         body:AuthModel.signupSchema,
@@ -25,16 +36,71 @@ export const app = new Elysia({prefix:"auth"})
             400:AuthModel.signupFailedSchema
         }
     })
-    .post("/sign-in",async ({body})=>{
-    
-        const token = await AuthService.signin(body.email,body.password)
-        return {
-            token
-        }
-    },{
-        body:AuthModel.signinSchema,
-        response:{
-            200:AuthModel.SigninResponseSchema,
-           403:AuthModel.SigninFailedSchema
+
+
+
+
+    .post("/sign-in",async ({body,jwt,cookie:{auth},status})=>{
+        const {correctCredentials,userId} = await AuthService.signin(body.email,body.password)
+        
+        if(correctCredentials&& userId){
+            const token = await jwt.sign({userId})
+            if(!auth){
+                auth = new Cookie("auth",{})
+            }
+            auth.set({
+                value: token,
+                httpOnly: true,
+                maxAge: 7 * 86400,
+            })
+            return {
+                message:"signed in successfully"
+            }
+            }else{
+                return status(403,{
+                    message:"incorrect creidentials"
+                })
+            }
+        
+        },
+            {
+        body: AuthModel.signinSchema,
+        response: {
+            200: AuthModel.SigninResponseSchema,
+            403: AuthModel.SigninFailedSchema
         }
     })
+        .resolve(async ({ cookie: { auth }, status, jwt}) => {
+        if (!auth) {
+            return status(401)
+        }
+
+        const decoded = await jwt.verify(auth.value as string);
+
+        if (!decoded || !decoded.userId) {
+            return status(401)
+        }
+
+        return {
+            userId: decoded.userId as string
+        }
+    })
+
+    // .get("/profile",async({userId,status})=>{
+    //     const userData = await AuthService.getUserDetail(Number(userId))
+    //     if(!userData){
+    //         return status(403,{
+    //             message:"Error While fetching user details"
+    //         })
+    //     }
+    //     return userData
+    // },{
+    //     response:{
+    //         200:AuthModel.profileResponseSchema,
+    //         403:AuthModel.profileResponseErrorSchema
+    //     }
+    // })
+        
+      
+       
+    
