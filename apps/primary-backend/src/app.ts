0b7476e9d979945f82b18bf4d894c98002c1cd12
Yash sharma <yashsharmaofficially@gmail.com>
2026-03-28 01:33:30 +0530

    finished the primary-backend

---
+++ apps/primary-backend/src/app.ts
@@ -0,0 +1,16 @@
+import {Elysia} from "elysia"
+import { app as authApp } from "./modules/auth";
+import {app as apiKeyApp} from "./modules/apiKeys"
+import {app as modelsApp} from "./modules/models"
+import {app as paymentsApp} from "./modules/payment"
+
+
+
+export const app = new Elysia()
+    .use(authApp)
+    .use(apiKeyApp)
+    .use(modelsApp)
+    .use(paymentsApp)
+
+
+export type App = typeof app
--- apps/primary-backend/src/index.ts
+++ apps/primary-backend/src/index.ts
@@ -1,14 +1,4 @@
-import { Elysia } from "elysia";
-import { app as authApp } from "./modules/auth";
-import {app as apiKeyApp} from "./modules/apiKeys"
-
-const app = new Elysia()
-  .use(authApp)
-  .use(apiKeyApp)
-  .listen(3000)
 
 
-console.log(
-  `🦊 Elysia is runningggg at ${app.server?.hostname}:${app.server?.port}`
-);
-
+import { app } from "./app";
+app.listen(3000)
