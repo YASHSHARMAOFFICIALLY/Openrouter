f8d45d77d021e6ba6f2b3b847f6bcad0a0ca477a
Yash sharma <yashsharmaofficially@gmail.com>
2026-03-28 14:23:51 +0530

    dashboard

--- apps/dashboard-frontend/src/App.tsx
+++ apps/dashboard-frontend/src/App.tsx
@@ -1,38 +1,26 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { APITester } from "./APITester";
+import type {App} from "app";
+import "./index.css"
+import {BrowserRouter,Route,Routes} from "react-router"
+import { Signin } from "./pages/Signin";
+import { Landing } from "./pages/Landing";
+import { Dashboard } from "./pages/Dashbaord";
 
-import logo from "./logo.svg";
-import reactLogo from "./react.svg";
 
-export function App() {
+export function App(){
   return (
-    <div className="container mx-auto p-8 text-center relative z-10">
-      <div className="flex justify-center items-center gap-8 mb-8">
-        <img
-          src={logo}
-          alt="Bun Logo"
-          className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#646cffaa] scale-120"
-        />
-        <img
-          src={reactLogo}
-          alt="React Logo"
-          className="h-36 p-6 transition-all duration-300 hover:drop-shadow-[0_0_2em_#61dafbaa] [animation:spin_20s_linear_infinite]"
-        />
-      </div>
-      <Card>
-        <CardHeader className="gap-4">
-          <CardTitle className="text-3xl font-bold">Bun + React</CardTitle>
-          <CardDescription>
-            Edit <code className="rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono">src/App.tsx</code> and save to
-            test HMR
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <APITester />
-        </CardContent>
-      </Card>
-    </div>
-  );
-}
+   <BrowserRouter>
+    <Routes>
+      <Route path ={"/"} element = {<Landing/>}/>
+      <Route path ={"signup"} element = {<Signin/>}/>
+      <Route path = {"signin"} element = {<Signin />}/>
+      <Route path = {"dashboard"} element = {<Dashboard/>}/>
 
+
+    </Routes>
+   
+   </BrowserRouter>
+  )
+
+}
 export default App;
+
