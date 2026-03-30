529cf6798c8919432967590b02bf10fffa241cd9
Yash sharma <yashsharmaofficially@gmail.com>
2026-03-30 17:53:17 +0530

    signin page

--- apps/dashboard-frontend/src/App.tsx
+++ apps/dashboard-frontend/src/App.tsx
@@ -3,6 +3,7 @@ import type {App} from "app";
 import "./index.css"
 import {BrowserRouter,Route,Routes} from "react-router-dom"
 import { LoginPage } from "./pages/Signin";
+import { SignupPage } from "./pages/Signup";
 import { Landing } from "./pages/Landing";
 import { Dashboard } from "./pages/Dashbaord";
 import { Suspense } from "react";
@@ -15,7 +16,7 @@ export function App(){
    <Suspense fallback={<div></div>}>
         <Routes>
           <Route path="/" element={<Landing/>}/>
-          <Route path="signup" element={<LoginPage/>}/>
+          <Route path="signup" element={<SignupPage/>}/>
           <Route path="signin" element={<LoginPage/>}/>
           <Route path="dashboard" element={<Dashboard/>}/>
         </Routes>
